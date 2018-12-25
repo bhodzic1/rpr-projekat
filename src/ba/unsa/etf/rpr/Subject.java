d0f9d7a7aadea90ea1564af9b29a0f586633c185
Bernes <bhodzic1@etf.unsa.ba>
2018-12-25 15:59:23 +0100

    Klasa predmet

---
+++ src/ba/unsa/etf/rpr/Subject.java
@@ -0,0 +1,75 @@
+package ba.unsa.etf.rpr;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.util.ArrayList;
+
+public class Subject {
+    private SimpleStringProperty name = new SimpleStringProperty("");
+    private SimpleIntegerProperty id = new SimpleIntegerProperty(0);
+    private SimpleStringProperty professorName = new SimpleStringProperty("");
+    private SimpleIntegerProperty semester = new SimpleIntegerProperty(0);
+    private ArrayList<Student> students = new ArrayList<>();
+
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getProfessorName() {
+        return professorName.get();
+    }
+
+    public SimpleStringProperty professorNameProperty() {
+        return professorName;
+    }
+
+    public void setProfessorName(String professorName) {
+        this.professorName.set(professorName);
+    }
+
+    public int getSemester() {
+        return semester.get();
+    }
+
+    public SimpleIntegerProperty semesterProperty() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester.set(semester);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) { //Register more students to subject
+        this.students = students;
+    }
+
+    public void setStudent(Student student) { //Register one student to subject
+        this.students.add(student);
+    }
+}
